e57129282b18651707f216b2a31e5e4e8c708a13
Masaki-ogr <69950491+Masaki-ogr@users.noreply.github.com>
2020-12-11 20:30:28 +0900

    バリデーションに共通オプションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+    devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true, length: { maximum: 40 }
- VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :email, format: { with: VALID_EMAIL_REGEX }
- PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
-  validates :password, format: { with: PASSWORD_REGEX }
- NAME_REGEX = /\A[ぁ-んァ-ヶ一-龥々]+\z/
-  validates :family_name, :first_name, presence:true, format: { with: NAME_REGEX }
- KANA_REGEX = /\A[ァ-ヶー－]+\z/
-  validates :family_name_kana, :first_name_kana, presence:true, format: { with: KANA_REGEX }
-  validates :birth, presence: true
+  with_options presence: true do
+    validates :nickname, length: { maximum: 40 }
+    validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+    validates :family_name, :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
+    validates :family_name_kana, :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birth
+  end
 end
